5be481442a63636bf88a202826f623a4e9f10f6c
lnoueryo <popo62520908@gmail.com>
2020-10-28 09:14:26 +0900

    プロフィール画面準備

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-{{--  <vue-canvas></vue-canvas>  --}}
 <br>
 <br>
 <br>
-{{--  <form id="image_form" action="{{ url('/api/create') }}" method="POST">  --}}
-<post></post>
-{{--  <crop-canvas></crop-canvas>  --}}
-    {{--  @csrf  --}}
-    {{--  <input id="image" type="hidden" name="image" value="">  --}}
-    {{--  <input id="submit_image" type="submit" name="submit" value="送信">
-</form>  --}}
-{{--  <index :latest-posted-users="{{ $latest_posted_users }}" :latest-posts="{{ $latest_posts }}"></index>  --}}
-{{--  <practice></practice>  --}}
+<profile></profile>
+{{--  <post></post>  --}}
 @endsection
