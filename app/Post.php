a626f2d8e120b5c71ea249f3a3e1f9bcc63f1a25
lnoueryo <popo62520908@gmail.com>
2020-12-22 22:53:40 +0900

    親テーブル、子テーブル、孫テーブルの関連付け
    Deleteで関連あるもの全て削除するため

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,37 @@ class Post extends Model
     protected $fillable = [
         'text','image',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function($post) {
+            // $likes = $post->likes;
+            // $comments = $post->comments;
+            // foreach($likes as $like) {
+            //     $like->delete();
+            // }
+            // foreach($comments as $comment) {
+            //     $comment->delete();
+            // }
+            foreach(['likes', 'comments'] as $relation) {
+                $comment->delete();
+            }
+        });
+    }
+
+    public function users()
+    {
+        return $this->hasMany('App\User', 'user_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like', 'post_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'post_id');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,60 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function($user) {
+            // $posts = $user->posts;
+            // $likes = $user->likes;
+            // $comments = $user->comments;
+            // $followees = $user->followees;
+            // $followers = $user->followers;
+            // foreach($posts as $post) {
+            //     $post->delete();
+            // }
+            // foreach($likes as $like) {
+            //     $like->delete();
+            // }
+            // foreach($comments as $comment) {
+            //     $comment->delete();
+            // }
+            // foreach($followees as $followee) {
+            //     $followee->delete();
+            // }
+            // foreach($followers as $follower) {
+            //     $follower->delete();
+            // }
+            foreach(['posts', 'likes', 'comments', 'followees', 'followers'] as $relation) {
+                $relation->delete();
+            }
+        });
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post', 'user_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like', 'user_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'user_id');
+    }
+
+    public function followees()
+    {
+        return $this->hasMany('App\Follower', 'followed_id');
+    }
+
+    public function followers()
+    {
+        return $this->hasMany('App\Follower', 'following_id');
+    }
+
 }
