cb00ed8743ad99cd5a927b46562a1a2a97b43856
lnoueryo <popo62520908@gmail.com>
2021-01-11 01:06:28 +0900

    関数型コンポーネント作成

---
+++ resources/ts/components/atoms/profile/user_posts/card_content/postTitle.vue
@@ -0,0 +1,18 @@
+<template functional>
+    <div>
+        <v-card-text class="py-1" style="position: relative; max-width: 340px;">
+            <p class="text-h6 font-weight-light orange--text mb-2" style="margin: 0!important;">
+            {{ props.postDialog.title }}
+            </p>
+            <div class="text-subtitle-1 font-weight-light grey--text title mb-2">
+                <div class="content">{{ props.postDialog.text }}</div>
+            </div>
+        </v-card-text>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['postDialog']
+}
+</script>
